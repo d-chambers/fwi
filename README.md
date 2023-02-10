9fddf979565568f4e396b58209818fd73fa28ee8
derrick chambers <djachambeador@gmail.com>
2023-02-10 16:48:21 -0700

    update

--- README.md
+++ README.md
@@ -4,4 +4,24 @@ CSM spring 2023 FWI course work.
 
 The homework directory contains subdirectories for each assignment.
 
-The src file contains a python package for working with specfem2d.
+The src file contains a python package for working with specfem2d and other
+utilities. To install this, do the following:
+
+
+# Specfem
+
+Compile [specfem2d](https://github.com/SPECFEM/specfem2d) and copy the bin
+in this directory.
+
+# Setup an environment
+
+```bash
+conda env create -f environment.yml
+conda activate fwi
+```
+
+# Install pre-commit
+
+```bash
+pre-commit install
+```
--- homework/assignment_3/part_2/UtilityFunctions.py
+++ homework/assignment_3/part_2/UtilityFunctions.py
@@ -6,6 +6,7 @@ Created on Mon Apr 25 10:09:05 2022
 @author: andreacamilarianoescandon using plot file seisflows-legacy
 """
 
+#!/usr/bin/env python
 
 import re
 
@@ -23,24 +24,16 @@ def read_trace(filename):
     # get station name from the filename
     net, sta, comp, *_ = filename.split("/")[-1].split(".")
     delta = times[1] - times[0]
-    headers = {
-        "station": sta,
-        "network": net,
-        "channel": comp,
-        "delta": delta,
-        "b": times[0],
-    }
+    headers = {"station": sta, "network": net, "channel": comp, "delta": delta, "b": times[0]}
     return obspy.Trace(disp, headers)
-
 
 def save_trace(tr, filename):
     """Writes out the traces as an ASCII file. Uses b value as the beginning."""
     data = np.zeros((len(tr.data), 2))
-    data[:, 0] = tr.times() + tr.stats.b
+    data[:, 0] = tr.times()+tr.stats.b
     data[:, 1] = tr.data
     np.savetxt(filename, data)
-
-
+       
 def specfem_write_parameters(filename, parameters, output_file=None):
     """Write parameters to a specfem config file"""
 
@@ -49,17 +42,16 @@ def specfem_write_parameters(filename, parameters, output_file=None):
 
     for varname, value in parameters.items():
         pat = re.compile(
-            r"(^{varname}\s*=\s*)([^#$\s]+)".format(varname=varname), re.MULTILINE
-        )
+            r"(^{varname}\s*=\s*)([^#$\s]+)".format(varname=varname),
+            re.MULTILINE)
         pars = pat.sub(r"\g<1>{value}".format(value=value), pars)
 
     if output_file is None:
         output_file = filename
 
     with open(output_file, "w") as f:
-        f.write(pars)
-
-
+        f.write(pars)     
+        
 def specfem2D_prep_save_forward(filename=None):
     if filename is None:
         filename = "./DATA/Par_file"
@@ -67,18 +59,19 @@ def specfem2D_prep_save_forward(filename=None):
         "SIMULATION_TYPE": 1,
         "SAVE_FORWARD": ".true.",
         "SAVE_MODEL": "binary",
-        "NGNOD": 9,
+        "NGNOD": 9
     }
     specfem_write_parameters(filename, params)
-
-
+    
 def specfem2D_prep_adjoint(filename=None):
     if filename is None:
         filename = "./DATA/Par_file"
-    params = {"SIMULATION_TYPE": 3, "SAVE_FORWARD": ".false."}
+    params = {
+        "SIMULATION_TYPE": 3,
+        "SAVE_FORWARD": ".false."
+    }
     specfem_write_parameters(filename, params)
-
-
+      
 def grid(x, y, z, resX=100, resY=100):
     """
     Converts 3 column data to matplotlib grid
@@ -90,18 +83,17 @@ def grid(x, y, z, resX=100, resY=100):
     yi = np.linspace(min(y), max(y), resY)
 
     # mlab version
-    # Z = griddata(x, y, z, xi, yi, interp='linear')
+    #Z = griddata(x, y, z, xi, yi, interp='linear')
     # scipy version
-    Z = griddata((x, y), z, (xi[None, :], yi[:, None]), method="cubic")
+    Z = griddata((x, y), z, (xi[None,:], yi[:,None]), method='cubic')
 
     X, Y = np.meshgrid(xi, yi)
     return X, Y, Z
 
-
 # AR: function to replace line in text file ------------------------------------
 def replace_line(file_name, line_num, text):
-    lines = open(file_name, "r").readlines()
+    lines = open(file_name, 'r').readlines()
     lines[line_num] = text
-    out = open(file_name, "w")
+    out = open(file_name, 'w')
     out.writelines(lines)
     out.close()
