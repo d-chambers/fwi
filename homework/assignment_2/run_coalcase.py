e3c8d4f293d489d2a334472762ec9dd9a775c177
derrick chambers <djachambeador@gmail.com>
2023-02-27 10:47:29 -0700

    update

--- homework/assignment_2/run_coalcase.py
+++ homework/assignment_2/run_coalcase.py
@@ -21,6 +21,14 @@ def read_stations(path="COALCASE1/OUTPUT_FILES/output_list_stations.txt"):
 def make_waveform_plot(st, event_location=(2000, 1500), amplification=120):
     """Make a plot of waveforms."""
 
+    def set_ylims(ax1, ax2):
+        y1 = ax1.get_ylim()
+        y2 = ax2.get_ylim()
+        min_low = min([y1[0], y2[0]])
+        max_high = max([y1[1], y2[1]])
+        ax1.set_ylim([min_low, max_high])
+        ax2.set_ylim([min_low, max_high])
+
     def get_distance(tr):
         """Get coordinates of the station of trace. """
         sta, net = tr.stats.station, tr.stats.network
@@ -44,11 +52,27 @@ def make_waveform_plot(st, event_location=(2000, 1500), amplification=120):
     x_traces = [tr for tr in st if tr.stats.channel == 'BXX']
     z_traces = [tr for tr in st if tr.stats.channel == "BXZ"]
     df = read_stations()
-    fig, ax = plot_traces(x_traces, color="blue")
-    fig.savefig("COALCASE1/OUTPUT_FILES/X_trace_plot.png")
-    #
-    fig, ax = plot_traces(z_traces, color="red")
-    fig.savefig("COALCASE1/OUTPUT_FILES/z_trace_plot.png")
+    fig1, ax1 = plot_traces(x_traces, color="blue")
+    fig2, ax2 = plot_traces(z_traces, color="red")
+    set_ylims(ax1, ax2)
+
+    fig1.savefig("COALCASE1/OUTPUT_FILES/X_trace_plot.png")
+    fig2.savefig("COALCASE1/OUTPUT_FILES/z_trace_plot.png")
+
+
+def plot_points(path="COALCASE1/OUTPUT_FILES/points_per_wavelength_histogram_S_in_solid.txt"):
+    """Plot the histogram of GLLs per wavelength. """
+    path = Path(path)
+    df = pd.read_csv(path, delim_whitespace=True, header=None)
+    df.columns = ['gll', 'percentage']
+    plt.close('all')
+    fig, ax = plt.subplots(1, 1)
+    # ax.plot(, df['gll'], '.')
+    ax.bar(df['gll'], df['percentage'], edgecolor='black', width=0.16)
+    ax.set_xlabel('GLL per S Wavelength')
+    ax.set_ylabel("% of Pixels")
+    fig.savefig("COALCASE1/OUTPUT_FILES/GLL_histogram.png")
+
 
 
 if __name__ == "__main__":
@@ -57,8 +81,17 @@ if __name__ == "__main__":
     if control.empty_output:
         control.xmeshfem2d()
         control.xspecfem2d()
+
+    # get waveforms, plot them
     st = control.get_waveforms()
+    # Apply bandpass filter based on output file recommendations
+    min_freq = 0.05
+    max_freq = 20.0
+    st_filtered = st.filter('bandpass', freqmin=min_freq, freqmax=max_freq)
     make_waveform_plot(st)
+
+    # plot the points
+    plot_points()
 
 
 
